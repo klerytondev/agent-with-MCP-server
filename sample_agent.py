0988f91cecaa19332bef4d7e33bd13123cf2f410
klerytondev <kleryton.dev@gmail.com>
2025-09-22 21:27:25 -0300

    adiciona funções para obter informações e preços de ações

--- sample_agent.py
+++ sample_agent.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
+from tools import get_company_info, get_history_stock_price, get_current_stock_price
 
 load_dotenv()
 
@@ -11,7 +12,9 @@ llm = ChatOpenAI(model="gpt-5-mini", temperature=0)
 memory = MemorySaver()
 systen_message = "You are a helpful assistant that helps people find information."
 
-tools = []
+tools = [get_company_info, 
+         get_history_stock_price, 
+         get_current_stock_price]
 
 agent_executor = create_react_agent(
     llm=llm,
--- tools.py
+++ tools.py
@@ -1,5 +1,29 @@
 from langchain_core.tools import tools
 import yfinance as yf
 
-def get_stock_price(ticker: str) -> str:
-    
+def get_history_stock_price(ticker: str, period: str) -> dict:
+	'''
+	Retorna o histórico de preços de uma ação com base no código da empresa e no período.
+	period : str
+		| Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+		| Default: 1mo
+		| Can combine with start/end e.g. end = start + period
+	'''
+	data = yf.Ticker(ticker)
+	return data.history(period=period).to_dict()
+
+def get_current_stock_price(ticker: str) -> float:
+	'''
+	Retorna valor atual de uma ação com base no código da empresa.
+	'''
+	data = yf.Ticker(ticker)
+	return data.fast_info.get("last_price")
+
+def get_company_info(ticker: str) -> dict:
+	'''
+	Retorna dados completos da empresa com base no código da empresa.
+	'''
+	data = yf.Ticker(ticker)
+	return data.info
+
+
